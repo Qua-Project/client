9164d99d8e0711a6dad72aec6acd6b5109482bac
kwonjeong <kwonjeong9470@gmail.com>
2025-03-04 00:17:03 +0900

    [chore] #4 - 불필요한 주석 제거

--- qua-app/src/components/around/ODSCardCommons/TotalRate.tsx
+++ qua-app/src/components/around/ODSCardCommons/TotalRate.tsx
@@ -3,11 +3,11 @@ import Svg, { Circle, Text } from "react-native-svg";
 import styled from "@emotion/native";
 
 export const TotalRate = ({ percentage = 80, total = 100 }) => {
-  const size = 80; // 전체 크기
-  const strokeWidth = 17.5; // 원의 두께
-  const radius = (size - strokeWidth) / 2; // 반지름
+  const size = 80;
+  const strokeWidth = 17.5;
+  const radius = (size - strokeWidth) / 2;
   const circumference = 2 * Math.PI * radius; // 전체 원 둘레
-  const progress = (percentage / total) * circumference; // 진행 부분
+  const progress = (percentage / total) * circumference;
   const strokeDashoffset = circumference - progress; // 12시 방향 시작
 
   return (
@@ -16,7 +16,6 @@ export const TotalRate = ({ percentage = 80, total = 100 }) => {
         {/* 배경 원 */}
         <StyledCircle cx={size / 2} cy={size / 2} r={radius} />
 
-        {/* 진행 원 */}
         <ProgressCircle
           cx={size / 2}
           cy={size / 2}
@@ -44,13 +43,11 @@ const RateWrapper = styled.View`
   width: 100%;
 `;
 
-/* 배경 원 */
 const StyledCircle = styled(Circle)`
   stroke-width: 17.5;
   fill: none;
 `;
 
-/* 진행 원 */
 const ProgressCircle = styled(Circle)`
   stroke: #79a3ff;
   stroke-width: 17.5;
@@ -60,12 +57,10 @@ const ProgressCircle = styled(Circle)`
   transform-origin: center;
 `;
 
-/* 중앙 흰색 원 */
 const CenterCircle = styled(Circle)`
   fill: ${({ theme }) => theme.colors.white};
 `;
 
-/* "Total" 텍스트 */
 const StyledText = styled(Text)`
   font-size: 10px;
   fill: ${({ theme }) => theme.colors.grey};
@@ -73,7 +68,6 @@ const StyledText = styled(Text)`
   text-anchor: middle;
 `;
 
-/* 퍼센트 숫자 */
 const StyledPercentage = styled(Text)`
   font-size: 18px;
   fill: ${({ theme }) => theme.colors.black};
