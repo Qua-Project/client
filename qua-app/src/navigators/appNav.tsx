7ca3a2219ea25a03dc2715d634dc12dbc8f2f479
kwonjeong <kwonjeong9470@gmail.com>
2025-03-04 00:18:00 +0900

    [chore] #4 - Around 스크린 뷰 화면 띄우기 (테스트)

--- qua-app/src/navigators/appNav.tsx
+++ qua-app/src/navigators/appNav.tsx
@@ -5,15 +5,16 @@ import LoginScreen from "../screens/onBoarding/login";
 import MainNavigator from "./mainNav";
 import OnBoardingNavigator from "./onBoardingNav";
 import { RootParamList } from "../types/type";
+import AroundScreen from "../screens/around/main";
 
 const Stack = createStackNavigator<RootParamList>();
 
 export default function AppNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={LoginScreen} />
+      {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
       {/* <Stack.Screen name="OnBoarding" component={OnBoardingNavigator} /> */}
+      <Stack.Screen name="Around" component={AroundScreen} />
     </Stack.Navigator>
   );
 }
-
